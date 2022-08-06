0b313b43f98086d5b7239c3175386f7f2c40f7fc
amir <galimzyanoffam@yandex.ru>
2022-08-06 18:32:39 +0300

    рефакторинг

--- app/Http/Controllers/Api/CardController.php
+++ app/Http/Controllers/Api/CardController.php
@@ -5,29 +5,18 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CardResource;
 use App\Models\Card;
-use App\Models\Desk;
 use App\Http\Requests\CardStoreRequest;
-use http\Env\Response;
-use Illuminate\Http\Request;
+
 
 class CardController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(CardStoreRequest $request)
     {
         $new_card = Card::create($request->validated());
@@ -35,24 +24,14 @@ class CardController extends Controller
         return new CardResource($new_card);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Card $card)
     {
         return new CardResource($card);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
+
     public function update(CardStoreRequest $request, Card $card)
     {
         $card->update($request->validated());
@@ -60,12 +39,7 @@ class CardController extends Controller
         return new CardResource($card);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Card $card)
     {
         $card->delete();
