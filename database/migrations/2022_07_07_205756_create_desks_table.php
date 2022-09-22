018ba896dca11e6784d8935c6a30ad530ccf0a9e
amir <galimzyanoffam@yandex.ru>
2022-09-22 08:38:35 +0300

    какая то хрень

--- database/migrations/2022_07_07_205756_create_desks_table.php
+++ database/migrations/2022_07_07_205756_create_desks_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('desks', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('user_id')->constrained();
+            $table->integer('user_id')->constrained();
             $table->timestamps();
         });
     }
